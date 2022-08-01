3d627ab821281fc8bdeed375694cff67b2aa7df8
george <george@intrepid-geophysics.com>
2022-08-01 15:17:29 +1000

    add search reverse proxy

---
+++ proxy/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+const secretConfig = require('./secret-config')
+
+const app = express()
+const {
+  PORT
+} = secretConfig
+
+app.use('/search', createProxyMiddleware({
+  target: 'http://localhost:3003',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/search':'/'
+  }
+}))
+
+if (secretConfig.USE_HTTPS) {
+  // Certificate
+  const privateKey = fs.readFileSync(secretConfig.privateKey, 'utf8')
+  const certificate = fs.readFileSync(secretConfig.cert, 'utf8')
+  const ca = fs.readFileSync(secretConfig.chain, 'utf8')
+
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca
+  }
+
+  const httpsServer = https.createServer(credentials, app)
+
+  httpsServer.listen(PORT, () => {
+    console.log(`HTTPS Server running on port ${PORT}`)
+  })
+} else {
+  const httpServer = http.createServer(app)
+
+  httpServer.listen(PORT, () => {
+    console.log(`HTTP Server running on port ${PORT}`)
+  })
+}
---
+++ proxy/secret-config-example.js
@@ -0,0 +1,6 @@
+exports.USE_HTTPS = false
+exports.PORT = 80
+
+exports.privateKey = 'privkey.pem'
+exports.cert = 'cert.pem'
+exports.chain = 'chain.pem'
